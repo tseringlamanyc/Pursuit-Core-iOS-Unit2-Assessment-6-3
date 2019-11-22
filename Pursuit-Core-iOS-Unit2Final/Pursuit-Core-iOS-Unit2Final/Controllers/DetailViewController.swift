a0943af26dffbae3b273157e7cd3145f0e042347
Tsering Lama <tseringlama@pursuit.org>
2019-11-22 10:28:09 -0500

    added two view controller

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Tsering Lama on 11/22/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
